a8f95d711cb3ec627f3e7f8bb7848b588d04c0f3
Eric Kim <hyeokjungkim@gmail.com>
2020-07-23 11:08:17 -0400

    adds third lecture

---
+++ 03-nested-data/flatiron.rb
@@ -0,0 +1,66 @@
+require 'pry'
+
+brooklyn = [
+    {
+        cohort_date: 42020,
+        name: "French Pry with Gitchup",
+        coaches: [
+            {
+                name: "Rei R",
+                foods: ["Durr Burger"]
+            },
+            {
+                name: "Jack H",
+                foods: ["Trader Joes Ice Cream"]
+            }
+        ],
+        lead: {
+            name: "Ian H",
+            pets: 2
+        }
+    },
+    {
+        cohort_date: 60120,
+        name: "Pryñatas",
+        coaches: [
+            {
+                name: "Ethan F",
+                foods: ["Sushi"]
+            },
+            {
+                name: "Otha H",
+                foods: ["Cheese"]
+            }
+        ],
+        lead: {
+            name: "Michelle R",
+            pets: 1
+        }
+    }
+]
+
+# Add one more cohort: {}
+
+# Add the cohort_date, name, coaches, lead 
+
+#     cohort_date: 71320,
+#     name: "NYC-04-SENG-FT-071320",
+#     coaches: [],
+#     lead: {}
+
+
+# Add Annie && Isabel to the coaches
+
+# Add Bubble Tea to Annie's list of foods
+
+# Add Mozzarella Cheese to Isabel's list of foods
+
+# Add Eric as the lead
+
+# Add pets as 0
+
+# Increment the number of pets
+
+
+binding.pry
+0
