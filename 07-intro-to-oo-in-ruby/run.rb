0b0c69e7ac2cdfa3fad104183bab0ce29cd1278b
sylwiavargas <sylwia.vargas@gmail.com>
2020-08-03 18:09:48 +0200

    during lecture

--- 07-intro-to-oo-in-ruby/run.rb
+++ 07-intro-to-oo-in-ruby/run.rb
@@ -3,6 +3,7 @@ require 'pry'
 class String
 
     def excited
+        binding.pry
         self.upcase + "!!!"
     end
 
@@ -10,8 +11,34 @@ end
 
 
 
+class Dog
+
+    def bark
+        puts "woof"
+    end
+
+    def sleeping
+        puts "zzzzz"
+    end
+end 
 
 
+floof = Dog.new
+
+class Animal
+end
+
+simba = Animal.new
 
 binding.pry
 0
+
+# Define object in programming domain
+#  Create a class and instantiate an instance of the class
+#  Add behavior to the instance of a class
+#  Explain the difference between a class and an instance
+
+
+
+# dry = "don't repeat yourself"
+
