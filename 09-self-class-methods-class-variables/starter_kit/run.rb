00e24fa1fef2d1b2b8e54f30e67d28702a9b716c
sylwiavargas <sylwia.vargas@gmail.com>
2020-08-04 12:40:06 +0200

    before lecture

--- 09-self-class-methods-class-variables/starter_kit/run.rb
+++ 09-self-class-methods-class-variables/starter_kit/run.rb
@@ -5,11 +5,18 @@ class Dog
     attr_reader :breed
     attr_accessor :fav_snack, :name
 
+    @@breed = ""
+
     def initialize(name_argument, fav_snack_argument, breed_argument)
         @name = name_argument
         @fav_snack = fav_snack_argument
-        @breed  = breed_argument
+        @breed = breed_argument
+        @@breed = @breed
         @number_of_legs = 4
+    end
+
+    def what_is_the_breed
+        @@breed
     end
 
     def run
@@ -30,8 +37,8 @@ class Dog
 
 end 
 
-fluff = Dog.new("Fluffy", "bones", "maltese")
-preciosa = Dog.new("Preciosa", "bacon", "corgi")
+lilo_the_maltese = Dog.new("Lilo", "bones", "maltese")
+chaos_the_corgi = Dog.new("Chaos", "bacon", "corgi")
 
 binding.pry
 0
