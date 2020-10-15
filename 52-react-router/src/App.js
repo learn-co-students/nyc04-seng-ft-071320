14d962937732153f88f96bfbd56f75fbda858b71
Eric Kim <hyeokjungkim@gmail.com>
2020-10-15 12:31:42 -0400

    adds lecture

--- 52-react-router/src/App.js
+++ 52-react-router/src/App.js
@@ -1,15 +1,109 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import NotFound from './NotFound'
 import Home from './Home'
 import Header from './Header'
 import Character from './CharacterComponents/Character'
+
+// Line below will look in the CharacterComponents folder
+  // Look for an index.js
 import { Cat, Horton, Lorax, Thing1, Thing2 } from './CharacterComponents';
 
-class App extends React.Component {
+
+
+// Routing stuff
+// When <Route> is rendered, we are putting components behind paths
+
+// When you wrap <Route> with a <Switch>, only one of the routes will ever show up
+
+// <Link > is a fancy anchor tag
+// < NavLink > is a fancy anchor tag, but when the URL matches the path, it has a className called "active"
+
+import { Route, Switch, Link, NavLink } from 'react-router-dom'
+
+// Way 1:
+  // <Route path="/cat" component={Cat} />
+
+// Way 2:
+  // <Route path="/cat"> <Cat ...send props /> </Route>
+
+// Way 3:
+  // <Route path="/cat" render={(routerProps) => <Cat ...send props/>}/>
+
+
+class App extends Component {
+
+  state = {
+    characters: []
+  }
+
+
+
+
+
+  componentDidMount(){
+    this.setState({
+      characters: [
+        {
+          id: 1,
+          name: "The Cat in the Hat",
+          gif: "https://media2.giphy.com/media/YrJ9irppjno2c/giphy.gif",
+          slug: "cat",
+        },
+        {
+          id: 2,
+          name: "Horton",
+          gif: "https://media0.giphy.com/media/DkVadqL1Sl5vi/giphy.gif",
+          slug: "horton",
+        },
+        {
+          id: 3,
+          name: "The Lorax",
+          gif: "https://media.giphy.com/media/RlE7socaGwjm/giphy.gif",
+          slug: "lorax"
+        },
+        {
+          id: 5,
+          name: "Thing2",
+          gif: "https://media1.tenor.com/images/e8583b865f497dbfdb3d1a35b22685c7/tenor.gif",
+          slug: "thing2",
+        },
+     ]
+    })
+  }
+
+
+
+
+
+  renderSpecificCharacter = (routerProps) => {
+    let whatUserTyped = routerProps.match.params.id
+    let foundCharacter = this.state.characters.find((characterPOJO) => {
+      return characterPOJO.id === parseInt(whatUserTyped)
+    })  
+    if(foundCharacter){
+      return <Character name={foundCharacter.name} gif={foundCharacter.gif}/>
+    } else {
+      return <NotFound />
+    }
+
+  }
+
+
+
 
 
   render() {
+
+    let arrayOfLinks = this.state.characters.map((characterPOJO) => {
+      return (
+        <NavLink 
+          key={characterPOJO.id} 
+          to={`/characters/${characterPOJO.id}`}>
+            {characterPOJO.name}
+        </NavLink>
+      )
+    })
 
     return (
       <div className="App">
@@ -17,22 +111,24 @@ class App extends React.Component {
           <Header/>
             <aside className="sidebar">
               <ul>
-                
+                { arrayOfLinks }
               </ul>
             </aside>
         </header>
 
         <main>
-          <Home />
-          <Cat/>
-          <Horton/>
-          <Lorax/>
-          <Thing1/>
-          <Thing2/>
 
+          <Switch>
+            {/* SPLASH/LANDING PAGE AT THE TOP */}
+            <Route path="/" exact component={Home} />
+
+            <Route path="/characters/:id" render={this.renderSpecificCharacter}/>
+            
+            {/* 404 PAGE AT THE BOTTOM */}
+            <Route component={NotFound} />
+          </Switch>
 
         </main>
-
       </div>
     );
   }
@@ -40,37 +136,3 @@ class App extends React.Component {
 
 
 export default App
-
-
-
-
-
-
-// state = {
-//   characters: [
-//     {
-//       id: 1,
-//       name: "The Cat in the Hat",
-//       gif: "https://media2.giphy.com/media/YrJ9irppjno2c/giphy.gif",
-//       slug: "cat",
-//     },
-//     {
-//       id: 2,
-//       name: "Horton",
-//       gif: "https://media0.giphy.com/media/DkVadqL1Sl5vi/giphy.gif",
-//       slug: "horton",
-//     },
-//     {
-//       id: 3,
-//       name: "The Lorax",
-//       gif: "https://media.giphy.com/media/RlE7socaGwjm/giphy.gif",
-//       slug: "lorax"
-//     },
-//     {
-//       id: 5,
-//       name: "Thing2",
-//       gif: "https://media1.tenor.com/images/e8583b865f497dbfdb3d1a35b22685c7/tenor.gif",
-//       slug: "thing2",
-//     },
-//  ]
-// }
--- 52-react-router/src/index.js
+++ 52-react-router/src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render( <App /> , document.getElementById('root'));
+// Import <BrowserRouter>
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render( 
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root')
+);
